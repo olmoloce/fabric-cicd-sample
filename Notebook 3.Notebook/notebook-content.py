327f6fc9a7061dcef312a59b17761054e2c49f3f
Olga Molocenco <43234781+olmoloce@users.noreply.github.com>
2024-12-18 17:33:49 +0200

    Committing 2 items from workspace a54726c8-088d-4723-8fda-f024123fa12d

---
+++ Notebook 3.Notebook/notebook-content.py
@@ -0,0 +1,29 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "7682a11f-ae2e-4f06-8210-8820a7214a77",
+# META       "default_lakehouse_name": "lakehouse_3",
+# META       "default_lakehouse_workspace_id": "a54726c8-088d-4723-8fda-f024123fa12d"
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+Print("Hello")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
